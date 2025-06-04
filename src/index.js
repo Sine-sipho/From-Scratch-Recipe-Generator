53e314f8aa4c26dae8f25928c86da9d10aa7469b
Sine-sipho <125162881+Sine-sipho@users.noreply.github.com>
2025-06-05 00:27:07 +0200

    API intergration using axios and basic css styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-const getInputValue = (event) => {
-  event.preventDefault();
-  let inputValue = document.querySelector("#text-input");
-
+const displayRecipe = (response) => {
   new Typewriter(".result", {
-    strings: "Let me not to the marriage of true minds admit impediments...",
+    strings: response.data.answer,
     autoStart: true,
     delay: 20,
     cursor: null,
   });
+};
+
+const getInputValue = (event) => {
+  event.preventDefault();
+  let inputValue = document.querySelector("#enquiry-input");
+
+  let apiKey = "34dcb3cfedt4654990206a21091oab73";
+  let context =
+    "You are knowledgeable food ingredients generating AI assistant for creating food from scratch, that can produce the ingredients for a variety of homemade recipes and the methods and instruction for a recipe. Write the lines in basic html format. Follow user instructions.";
+  let =
+    prompt = `User instructions are: Generate a recipe about ${inputValue.value}`;
+  let apiLink = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiLink).then(displayRecipe);
 };
 
 let enquiryFormElement = document.querySelector(".enquiry-form");
