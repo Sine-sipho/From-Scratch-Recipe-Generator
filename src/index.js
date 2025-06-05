f2b2a9c1bc0f3de8c474c50903d5e934c9c42d96
Sine-sipho <125162881+Sine-sipho@users.noreply.github.com>
2025-06-05 17:56:07 +0200

    improved user experience by adding css styling

--- src/index.js
+++ src/index.js
@@ -13,12 +13,22 @@ const getInputValue = (event) => {
 
   let apiKey = "34dcb3cfedt4654990206a21091oab73";
   let context =
-    "You are knowledgeable food ingredients generating AI assistant for creating food from scratch, that can produce the ingredients for a variety of homemade recipes and the methods and instruction for a recipe. Write the lines in basic html format. Follow user instructions.";
+    "You are knowledgeable food ingredients generating AI assistant for creating food from scratch, that can produce the ingredients for a variety of homemade recipes and the methods and instruction for a recipe. Generate the lines using basic html. Follow user instructions.";
   let =
     prompt = `User instructions are: Generate a recipe about ${inputValue.value}`;
   let apiLink = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiLink).then(displayRecipe);
+
+  let element = document.querySelector(".result");
+  element.classList.add("hidden");
+
+  new Typewriter(".query", {
+    strings: "Please be patient, we are currently generating your recipe.",
+    autoStart: true,
+    delay: 10,
+    cursor: null,
+  });
 };
 
 let enquiryFormElement = document.querySelector(".enquiry-form");
